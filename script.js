166be0cf41d7900c779ab27c59b68b7e2363222d
Gabriela Nebbia <gabrielanebbia@gmail.com>
2021-12-06 23:50:55 -0300

    feat: Cria funcao para adiciona comida

--- script.js
+++ script.js
@@ -3,35 +3,45 @@ let context = canvas.getContext("2d");
 let box = 32;
 
 let snake = []; 
-snake[0] ={
+snake[0] = {
     x: 8 * box,
     y: 8 * box
-}
+};
 
 let direction = "right";
 
-function createBG(){
+let food = {
+  x: Math.floor(Math.random() * 15 + 1) * box,
+  y: Math.floor(Math.random() * 15 + 1) * box
+};
+
+function createBG() {
   context.fillStyle = "lightgreen";
   context.fillRect(0, 0, 16*box, 16*box);
 }
 
-function createSnake (){
+function createSnake() {
   for(i = 0; i < snake.length; i++){
     context.fillStyle = "green";
     context.fillRect(snake[i].x, snake[i].y, box, box);
   }
 }
 
+function drawFood() {
+  context.fillStyle = "red";
+  context.fillRect(food.x, food.y, box, box);
+}
+
 document.addEventListener('keydown', update);
 
-function update(event){
+function update(event) {
   if(event.keyCode == 37 && direction != 'right') direction = 'left';
   if(event.keyCode == 38 && direction != 'down') direction = 'up';
   if(event.keyCode == 39 && direction != 'left') direction = 'right';
   if(event.keyCode == 40 && direction != 'up') direction = 'down';
 }
 
-function startGame(){    
+function startGame() {    
   if(snake[0].x > 15*box && direction == "right") snake[0].x = 0;
   if(snake[0].x < 0 && direction == 'left') snake[0].x = 16 * box;
   if(snake[0].y > 15*box && direction == "down") snake[0].y = 0;
@@ -39,6 +49,7 @@ function startGame(){
 
   createBG();
   createSnake();
+  drawFood();
 
   let snakeX = snake[0].x;
   let snakeY = snake[0].y;
@@ -50,10 +61,10 @@ function startGame(){
 
   snake.pop(); 
 
-  let newHead ={
+  let newHead = {
       x: snakeX,
       y: snakeY
-  }
+  };
 
   snake.unshift(newHead);
 }
